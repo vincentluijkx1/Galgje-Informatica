1c6055f511fa0f6194c19c99c9934adcda25502b
Vincent Luijkx <93121307+vincentluijkx1@users.noreply.github.com>
2022-04-22 07:45:40 +0000

    altijd woord 1 kiezen

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Welkom bij galgje!")
 
 woorden = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
 
+gekozenwoord = woorden[1]
+
+print(gekozenwoord)
