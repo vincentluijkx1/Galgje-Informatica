452d83b57f1802ba2a2ca69bfcf7562b48fb7f05
Nithush <93372131+van-der-NithusH-II@users.noreply.github.com>
2022-05-25 10:29:33 +0000

    Bug fix en je kan meerdere letters gokken nu.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import re
+import random
+
 print("Welkom bij galgje!")
 
 print ()
@@ -8,18 +11,28 @@ print("Hallo, %s we gaan nu beginnen met galgje!" % (naam))
 import random
 woorden = ("informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk")
 randomWoord = random.choice(woorden)
-goed = woorden
 streepjes = []
-
+goed = []
 print( )
+while True:
+  gekozenletter = input("Kies een letter: ")
+  for letter in randomWoord:
+    if gekozenletter in randomWoord and gekozenletter == letter or letter in goed: 
+      streepjes.append(letter)
+      if letter not in goed:
+        goed.append(letter)
+    else:
+      streepjes.append("_")
+  print(streepjes)
+  streepjes = []
+  print( )
 
-gekozenletter = input("Kies een letter: ")
-for letter in randomWoord:
-  if gekozenletter in randomWoord and gekozenletter == letter: 
-    streepjes.append(letter)
-  else:
-    streepjes.append("_")
+
+  
 
 
-print(streepjes)
+
+  
+
+
 
