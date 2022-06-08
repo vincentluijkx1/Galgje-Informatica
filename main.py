1a70146d03047b04bd97ed8baa8d78f04e9d4755
Nithush <93372131+van-der-NithusH-II@users.noreply.github.com>
2022-06-08 12:54:46 +0000

    Keus voor de speler om opnieuw te spelen toegevoegd

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ galgVisual = {
 }
 
 def gameloop():
-  global fouten, run, woord, streepjes
+  global fouten, run, woord, streepjes, randomWoord, goed
   gekozenletter = input("Kies een letter: \n")  
   print( )
   for letter in randomWoord:
@@ -105,29 +105,45 @@ def gameloop():
     fouten += 1
     galgFouten = str(fouten)
     print(galgVisual[galgFouten])
+
   if gekozenletter.isdigit():
     print("Dat is geen letter!")
     fouten -=1
   if gekozenletter == randomWoord or not "_" in streepjes:
     print("Goedzo %s je hebt het woord geraden!" % (naam))
-    print("Het woord was " + randomWoord)
-    run = False
-    fouten = 0
+    print("Het woord was " + randomWoord) 
+    fouten = 9
   else:
     print(" ".join(streepjes))
-    print("%s fout" % fouten)
-
-
-  woord = "".join(streepjes)
+    print("%s fout" % fouten) 
 
   streepjes = []
+  
   print( )
-  if fouten >8: 
-   print("Helaas, je hebt verloren %s." % (naam))
-   print( )
-   print("Het woord was " + randomWoord + ".")
-   run = False
-   fouten = 0
+  if fouten > 8: 
+    print("Helaas, je hebt verloren %s." % (naam))
+    print( )
+    print("Het woord was " + randomWoord + ".")
+    keus = input("nog een keer?")
+    if(keus == "ja"):
+      randomWoord = random.choice(woorden)
+      streepjes = []
+      goed = []
+      fouten = 0
+    else:
+      exit()
+
 
 while run == True:
-  gameloop()
+  while fouten < 9:
+    gameloop()
+  randomWoord = random.choice(woorden)
+  streepjes = []
+  goed = []
+  fouten = 0
+
+  
+
+
+
+ 
