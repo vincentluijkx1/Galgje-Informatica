a13f11626b66ee84d79a26a151e8347b1b48b7c5
Vincent Luijkx <93121307+vincentluijkx1@users.noreply.github.com>
2022-06-10 07:51:06 +0000

    code netjes gamaakt (comments)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+#start
 print("Welkom bij galgje!")
 
 print ()
@@ -7,7 +8,7 @@ naam = input ("Hoe heet je?: ")
 print( )
 print("Hallo, %s we gaan nu beginnen met galgje!" % (naam))
 
-
+#woordenlijst en kiest randomwoord
 woorden = ("informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk")
 randomWoord = random.choice(woorden)
 toonFouteWoorden = []
@@ -16,6 +17,7 @@ goed = []
 fouten = 0
 run = True
 
+#galg
 galgVisual = {
 	"1": '''
                                           	  
@@ -91,8 +93,11 @@ galgVisual = {
 	'''
 }
 
+#def vanaf hier begint het 
 def gameloop():
   global fouten, run, woord, streepjes, randomWoord, goed
+
+  #letter input
   gekozenletter = input("Kies een letter: \n")  
   print( )
   for letter in randomWoord:
@@ -102,6 +107,8 @@ def gameloop():
         goed.append(letter)
     else:
       streepjes.append("_") 
+
+    #if letter niet goed
   if gekozenletter not in randomWoord :
     fouten += 1
     galgFouten = str(fouten)
@@ -110,14 +117,19 @@ def gameloop():
     print( )
     print("Foute letters:", *toonFouteWoorden)
 
+  #als je getal invuld
   if gekozenletter.isdigit():
     print("Dat is geen letter!")
     fouten -=1
+
+    #als je gewonnen hebt
   if gekozenletter == randomWoord or not "_" in streepjes:
     print("Goedzo %s je hebt het woord geraden!" % (naam))
     print("Het woord was " + randomWoord) 
     print( )
     fouten = 9
+
+    #keus om opnieuw te spelen
     keus = input("nog een keer?  ja/nee")
     if(keus == "ja"):
       randomWoord = random.choice(woorden)
@@ -128,19 +140,21 @@ def gameloop():
       print("Bedankt voor het spelen")
       exit()
 
-      
+    # _ in plaats van vierkantjes
   else:
     print(" ".join(streepjes))
     print("%s fout" % fouten) 
-
   streepjes = []
-  
+
+  #als je hebt verloren
   print( )
   if fouten > 8: 
     print("Helaas, je hebt verloren %s." % (naam))
     print( )
     print("Het woord was " + randomWoord + ".")
     print( )
+
+   #keuze om opnieuw te spelen
     keus = input("nog een keer?  ja/nee")
     if(keus == "ja"):
       randomWoord = random.choice(woorden)
@@ -151,7 +165,7 @@ def gameloop():
       print("Bedankt voor het spelen")
       exit()
 
-
+    #gameloop
 while run == True:
   while fouten < 9:
     gameloop()
